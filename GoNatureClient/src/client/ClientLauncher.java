bdf095e87378d723535b8ff780c65ab23aef6488
layanb3 <layanb3@10.0.0.8>
2026-06-25 21:43:09 +0300

    Adding javadoc for client package

--- GoNatureClient/src/client/ClientLauncher.java
+++ GoNatureClient/src/client/ClientLauncher.java
@@ -1,5 +1,8 @@
 package client;
 
+/**
+ * Launcher class for the GoNature client application.
+ */
 public class ClientLauncher {
     public static void main(String[] args) {
         ClientUI.main(args);
